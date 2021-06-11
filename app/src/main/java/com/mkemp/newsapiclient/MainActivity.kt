ad9474249d28d6c036f564e9bedc1295656ebd86
MKemp <mkemp@accu-time.com>
2021-06-11 15:56:22 -0400

    Add navigation graph and fragments

--- app/src/main/java/com/mkemp/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/mkemp/newsapiclient/MainActivity.kt
@@ -2,12 +2,23 @@ package com.mkemp.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.mkemp.newsapiclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bnvNews.setupWithNavController(navController)
     }
 }
