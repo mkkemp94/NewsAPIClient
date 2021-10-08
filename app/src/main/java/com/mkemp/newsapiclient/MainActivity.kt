584b5ae3324d17266a7187bb30d4b0c0f8eef269
MKemp <mkemp@accu-time.com>
2021-10-08 16:50:38 -0400

    Add navigation graph and fragments, BIG BUG FIX: Show recycler view above progress bar!!

--- app/src/main/java/com/mkemp/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/mkemp/newsapiclient/MainActivity.kt
@@ -2,12 +2,21 @@ package com.mkemp.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.mkemp.newsapiclient.databinding.ActivityMainBinding
+import com.mkemp.newsapiclient.presentation.viewmodel.NewsViewModel
+import com.mkemp.newsapiclient.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity()
 {
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    lateinit var viewModel: NewsViewModel
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?)
@@ -18,7 +27,8 @@ class MainActivity : AppCompatActivity()
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
         val navController = navHostFragment.navController
-
         binding.bnvNews.setupWithNavController(navController)
+
+        viewModel = ViewModelProvider(this, factory).get(NewsViewModel::class.java)
     }
 }
